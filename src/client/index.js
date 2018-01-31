cc25914d734d8cdaa7036f3e40d1c6cf24f1fa60
Przemysław Krasiński <przemek.krasinski@gmail.com>
2018-01-31 17:42:24 +0100

    initial commit

---
+++ src/client/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import App from './App'
+import Hello from './components/Hello'
+
+Vue.config.debug = true
+Vue.use(Router)
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    { name: 'hello', path: '/hello', component: Hello }
+  ]
+})
+
+new Vue({
+  el: '#app',
+  router,
+  render (createElement) {
+    return createElement(App)
+  }
+})
